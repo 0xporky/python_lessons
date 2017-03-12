be713ce2404f1e1f237308b762f9111c98f2b12a
Andrew Komar <0xporky@gmail.com>
2017-03-12 21:35:12 +0000

    Task 6.3a.py.

---
+++ 6/6.3a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    with open('CAM_table.txt', 'r') as f:
+        lines = [line for line in f.read().split('\n')
+                 if '.' in line]
+
+    lines.sort()
+    for line in lines:
+        print(line)
